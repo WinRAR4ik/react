4daed0dca818aa80b27d7ac157e2912a9f20b235
Mark Elan <killorden@gmail.com>
2024-07-22 00:38:35 +0300

    🔥Final

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,8 +1,33 @@
 import './styles.scss';
+
 export function About() {
   return (
     <div className='AboutPage'>
       <h1 className='title'>Sobre o projecto</h1>
+      <p className='description'>
+        Este site é um projeto desenvolvido como parte da minha Prova de Aptidão
+        Profissional (PAP) para o Curso de Informática da Escola Miguel Torga.
+      </p>
+      <p className='description'>
+        O principal objetivo deste site é demonstrar os conhecimentos e
+        habilidades adquiridos durante o curso. É importante notar que este
+        projeto foi criado exclusivamente para fins educacionais e de avaliação.
+      </p>
+      <p className='description'>
+        Este site não foi projetado ou otimizado para publicação na internet e
+        uso público. Todos os conteúdos, funcionalidades e design foram
+        desenvolvidos e podem não refletir as melhores práticas para sites
+        comerciais ou públicos.
+      </p>
+      <p className='description'>
+        Por favor, considere este site como uma demonstração das minhas
+        capacidades técnicas e criativas no contexto do meu curso de
+        Informática.
+      </p>
+      <p className='description'>
+        Agradeço a sua compreensão e interesse no meu projeto.
+      </p>
+      <img className={'image'} src='/school.jpg' alt='' />
     </div>
   );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,15 @@ export function Home() {
   return (
     <div className='Home'>
       <h1 className='Title'>Bem vindos no meu site Online Galleria</h1>
+      <p className='Description'>
+        Este site foi criado para apreciar a arte e dá uma possibilidade
+        fantástica para as pessoas que, por vários motivos, não conseguem
+        apreciar a arte ao vivo.
+      </p>
+      <div className='img-wrap'>
+        <img className={'image'} src='/sleepy-head.jpg' alt='' />
+        <img className={'image'} src='/handsome_businessman.jpg' alt='' />
+      </div>
     </div>
   );
 }
