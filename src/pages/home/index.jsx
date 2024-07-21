ab31d0a27aa52615d2159ee7684ed5936161fb4f
WinRAR4ik <grudinskisvlad@gmail.com>
2024-07-21 23:36:57 +0100

    fix

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,9 +4,12 @@ export function Home() {
     <div className='Home'>
       <h1 className='Title'>Bem-vindos ao meu site Online Galleria</h1>
       <p className='Description'>
-        Este site foi criado para apreciar a arte e dá uma possibilidade
-        fantástica para as pessoas que, por vários motivos, não conseguem
-        apreciar a arte ao vivo.
+        Este site foi criado para apreciar a arte. Através de tecnologias
+        avançadas e recursos interativos, este site oferece uma experiência
+        imersiva que permite aos visitantes explorar obras de arte de todo o
+        mundo, desde pinturas clássicas até instalações contemporâneas. E tambem
+        não apenas remove barreiras geográficas, mas também proporciona acesso a
+        pessoas com mobilidade reduzida ou que não tem tempo para ver ao vivo.
       </p>
       <div className='img-wrap'>
         <img className={'image'} src='/sleepy-head.jpg' alt='' />
